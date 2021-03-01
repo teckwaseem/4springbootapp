6cb2929f87b9478ad2470056aa1256feece6227a
teckwaseem <WT@gmail.com>
2021-03-02 03:52:54 +0530

    n nvbn

--- 4springbootapp/src/main/java/com/example/demo/Application.java
+++ 4springbootapp/src/main/java/com/example/demo/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+int x=30;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
